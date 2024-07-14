cd8e77c99226e9b49bd1265103ef0152fcf38d34
Chaipat J. <work.chaipat@gmail.com>
2024-07-14 20:24:58 +0700

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -5,8 +5,20 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-    socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`));
-    socket.end();
+    socket.on("data", (data) => {
+        const message = data.toString();
+        const [method, path] = message.split(" ");
+
+        let response = "HTTP/1.1 "
+        if (method === "GET" && path === "/") {
+            response += "200 OK";
+        } else {
+            response += "404 Not Found";
+        }
+        const responseBody = response + "\r\n\r\n";
+        socket.write(responseBody);
+    });
+
 });
 
 server.listen(4221, "localhost");
