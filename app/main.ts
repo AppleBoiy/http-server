e0bfa9aae37b0222580c2921b742f538ed6f2c11
Chaipat J. <work.chaipat@gmail.com>
2024-07-14 19:05:46 +0700

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -5,9 +5,8 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.on("close", () => {
+    socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`));
     socket.end();
-  });
 });
 
 server.listen(4221, "localhost");
