a817d2b0d825f96b16186ad83e558ff341a3d8d8
Chaipat J. <work.chaipat@gmail.com>
2024-07-14 20:31:20 +0700

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -6,27 +6,42 @@ const server = net.createServer((socket) => {
         const [requestLine] = message.split("\r\n");
         const [method, path] = requestLine.split(" ");
 
-        if (method === "GET" && path.startsWith("/echo/")) {
-            const echoStr = path.slice(6); // Extract the string from the path
-            const contentType = "text/plain";
-            const contentLength = Buffer.byteLength(echoStr);
-
-            // Construct the response headers and body
-            const response =
-                `HTTP/1.1 200 OK\r\n` +
-                `Content-Type: ${contentType}\r\n` +
-                `Content-Length: ${contentLength}\r\n` +
-                `\r\n` +
-                `${echoStr}`;
-
-            socket.write(response);
-        } else {
+        if (method !== "GET") {
             const response =
                 `HTTP/1.1 404 Not Found\r\n` +
                 `Content-Type: text/plain\r\n` +
                 `Content-Length: 13\r\n` +
                 `\r\n` +
                 `404 Not Found`;
+
+            socket.write(response);
+        } else if (path === "/") {
+            // Construct the response headers and body
+            const contentType = "text/html";
+            const content = "<h1>Hello, World!</h1>";
+            const contentLength = Buffer.byteLength(content);
+
+            const response =
+                `HTTP/1.1 200 OK\r\n` +
+                `Content-Type: ${contentType}\r\n` +
+                `Content-Length: ${contentLength}\r\n` +
+                `\r\n` +
+                `${content}`;
+
+            socket.write(response);
+
+        } else if (path.startsWith("/echo")) {
+            // Construct the response headers and body
+            const echoStr = path.slice(6); // Extract the string from the path
+            const contentType = "text/plain";
+            const contentLength = Buffer.byteLength(echoStr);
+
+            const response =
+                `HTTP/1.1 200 OK\r\n` +
+                `Content-Type: ${contentType}\r\n` +
+                `Content-Length: ${contentLength}\r\n` +
+                `\r\n` +
+                `${echoStr}`;
 
             socket.write(response);
         }
